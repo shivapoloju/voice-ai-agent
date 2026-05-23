1a7000b75446f03dcad2d941ba3b567ffa6afa35
shivapoloju <shivasaipoloju5@gmail.com>
2026-05-23 12:41:59 +0530

    Add import for threading in email_service.py

--- email_service.py
+++ email_service.py
@@ -2,6 +2,7 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+
 from datetime import datetime, timedelta
 import schedule
 import time
